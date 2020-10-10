b69633563e53b49a09bc329a9b39f6c63d784d84
Joseph Tran <joseph.tran.p@gmail.com>
2020-10-10 16:00:53 +1100

    Add player and tiles

---
+++ Assets/Scripts/TileRandomiser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileRandomiser : MonoBehaviour
+{
+    public Sprite[] tileSprites;
+    private SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        var rand = Random.Range(0, tileSprites.Length);
+        sr.sprite = tileSprites[rand];
+    }
+}
