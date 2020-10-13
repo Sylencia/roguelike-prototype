00cd691f97076beaf122f899aabf8500b52fc627
Joseph Tran <joseph.tran.p@gmail.com>
2020-10-14 00:04:25 +1100

    Add basic obstacle and death effect

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health;
+    public GameObject deathEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle") {
+            health -= 1;
+        }
+    }
+
+    private void Update()
+    {
+        if(health == 0) {
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
