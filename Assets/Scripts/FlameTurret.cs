065bc329979ee11e7602d743bc7359a78802e540
Patrick Haikal <pathaikal@gmail.com>
2020-10-16 14:16:36 +1100

    Added a flame trap

---
+++ Assets/Scripts/FlameTurret.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameTurret : MonoBehaviour
+{
+    public ParticleSystem flameEmitter;
+    public float flameOnTime;
+    public float flameOffTime;
+
+    private BoxCollider2D boxCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        StartCoroutine(ControlFlames());
+    }
+
+    private IEnumerator ControlFlames()
+    {
+        flameEmitter.Play();
+        boxCollider.enabled = true;
+        yield return new WaitForSeconds(flameOnTime);
+        flameEmitter.Stop();
+        boxCollider.enabled = false;
+        yield return new WaitForSeconds(flameOffTime);
+        StartCoroutine(ControlFlames());
+    }
+}
